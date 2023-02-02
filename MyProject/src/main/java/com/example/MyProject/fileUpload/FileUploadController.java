5e069b94a34381dc76b53521c4dccfb9d615b737
Olgun <emre.olgun@email.com>
2023-02-02 11:52:43 +0100

    Get file by filename (Complete)

--- MyProject/src/main/java/com/example/MyProject/fileUpload/FileUploadController.java
+++ MyProject/src/main/java/com/example/MyProject/fileUpload/FileUploadController.java
@@ -1,13 +1,20 @@
 package com.example.MyProject.fileUpload;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.core.io.InputStreamResource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartRequest;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
 
 @RestController
 @RequestMapping(path = "api/v1/file")
@@ -18,6 +25,31 @@ public class FileUploadController {
     @PostMapping
     public void uploadFile(@RequestParam("file") MultipartFile file) throws IOException {
         fileUploadService.uploadFile(file);
+    }
+
+    @GetMapping(value = "/{fileName}")
+    @ResponseBody
+    public final ResponseEntity<InputStreamResource> getFileByName(@PathVariable("fileName") String fileName) throws Exception {
+
+        String folderPath = "C:\\Users\\emreo\\Projects\\MySpringProject\\MyProject\\uploadedFilesStorage\\"; //TODO: start path from current directory
+
+        File initialFile = new File(folderPath+fileName);
+        InputStream targetStream = new FileInputStream(initialFile);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.valueOf("video/mp4"));
+        headers.set("Accept-Ranges", "bytes");
+        headers.set("Expires", "0");
+        headers.set("Cache-Control", "no-cache, no-store");
+        headers.set("Connection", "keep-alive");
+        headers.set("Content-Transfer-Encoding", "binary");
+
+        return new ResponseEntity<>(new InputStreamResource(targetStream), headers, HttpStatus.OK);
+    }
+
+    @GetMapping(path="/all")
+    public void getVideos() {
+        
     }
 
 }
